88f5cf802ec8111c6a0351b4ca22255f06d57f1c
iishanmakkar <ishanmakkar651@gmail.com>
2026-07-28 17:13:56 +0530

    Remove social login buttons except Google

--- frontend/src/pages/SocialSignupButtons.jsx
+++ frontend/src/pages/SocialSignupButtons.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebookF, FaGithub } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 
 const SocialLoginButtons = () => {
   return (
@@ -14,15 +12,6 @@ const SocialLoginButtons = () => {
         >
           <FcGoogle className="text-2xl" />
         </a>
-        <button className="p-3 rounded-full hover:bg-[#e6f0fb] hover:shadow-[0_0_20px_5px_rgba(24,119,242,0.7)] transition duration-300 ease-in-out">
-          <FaFacebookF className="text-2xl text-[#1877F2]" />
-        </button>
-        <button className="p-3 rounded-full hover:bg-[#f1f1f1] hover:shadow-[0_0_20px_5px_rgba(0,0,0,0.3)] transition duration-300 ease-in-out">
-          <FaGithub className="text-2xl text-black" />
-        </button>
-        <button className="p-3 rounded-full hover:bg-[#e8f4fc] hover:shadow-[0_0_20px_5px_rgba(29,155,240,0.7)] transition duration-300 ease-in-out">
-          <FaXTwitter className="text-2xl text-black" />
-        </button>
       </div>
     </div>
   );
